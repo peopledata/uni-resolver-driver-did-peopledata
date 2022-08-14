54e441732712d0bdc2915ab3ae1b72568e592ff6
jerry.zhang <jerry.zhang.bill@gmail.com>
2022-08-14 12:23:11 +0800

    minor

--- controllers/Default.js
+++ controllers/Default.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var utils = require('../utils/writer.js');
-var Default = require('../service/DefaultService');
+var ResolveService = require('../service/ResolveService');
 
 module.exports.resolve = function resolve (req, res, next, identifier, accept) {
-  Default.resolve(identifier, accept)
+  ResolveService.resolve(identifier, accept)
     .then(function (response) {
       utils.writeJson(res, response);
     })
--- service/ResolveService.js
+++ service/ResolveService.js
@@ -8,23 +8,25 @@
  * accept String The requested MIME type of the DID document or DID resolution result. (optional)
  * returns Object
  **/
+
+
 exports.resolve = function(identifier,accept) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['did:peopledata:0000000000123456'] = {
+    examples['did:peopledata:123456789'] = {
       "@context": "https://www.w3.org/2019/did/v1",
-      "id" : "did:peopledata:0000000000123456",
-      "publicKey" : [ {
-        "id" : "did:peopledata:0000000000123456#key-1",
-        "type" : "Ed25519VerificationKey2018",
-        "publicKeyBase58" : "H3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV"
+      "id": "did:peopledata:123456789",
+      "publicKey": [ {
+        "id": "did:peopledata:123456789#key-1",
+        "type": "Ed25519VerificationKey2018",
+        "publicKeyBase58": "H3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV"
       } ]
     };
-    examples['did:peopledata:0000000000456789'] = {
+    examples['did:peopledata:5678'] = {
       "@context": "https://www.w3.org/2019/did/v1",
-      "id" : "did:peopledata:0000000000456789",
+      "id" : "did:peopledata:5678",
       "publicKey" : [ {
-        "id" : "did:peopledata:0000000000456789#key-1",
+        "id" : "did:peopledata:5678#key-1",
         "type" : "Ed25519VerificationKey2018",
         "publicKeyBase58": "EKmHWjiDDeMhEBs75uv86dC89zZ6yTTxEAHkYroJ7wZP"
       } ]
